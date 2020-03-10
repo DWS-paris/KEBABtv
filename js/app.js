1065c5cccf8d80a9e2656407e7eb211d9afc2947
Julien Noyer <j.noyer@digitalworkshop.fr>
2020-03-10 13:22:45 +0100

    DOM

--- js/app.js
+++ js/app.js
@@ -5,8 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     /* 
     Déclarations
-    */
-        const apiUrl = 'https://api.dwsapp.io';
+    */  
+        const localSt = 'qsekjh';
+        const mainNav = document.querySelector('header nav');
+        const apiUrl = 'http://localhost:8898';
         const registerForm = document.querySelector('#registerForm');
         const userEmail = document.querySelector('[name="userEmail"]');
         const userPassword = document.querySelector('[name="userPassword"]');
@@ -22,21 +24,26 @@ document.addEventListener('DOMContentLoaded', () => {
         const themoviedbUrl = 'https://api.themoviedb.org/3/search/movie?api_key=6fd32a8aef5f85cabc50cbec6a47f92f&query=';
         const movieList = document.querySelector('#movieList');
         const moviePopin = document.querySelector('#moviePopin article');
+        const favoriteList = document.querySelector('#favorite ul');
+        const loading = document.querySelector('#loading');
+
+        // https://vsrequest.video/request.php?key=oh61giHx16GXKSIz&secret_key=h9oe5ezesk0c0f9zk143zpkd7tba63&video_id=348&tmdb=1&ip=77.204.106.81
     //
 
     /* 
     Fonctions
     */
-        const checkUserToken = () => {
-            fetch( `${apiUrl}/api/me` )
-            .then( apiResponse => {
-                return apiResponse.json()
-            })
-            .then(data => console.log(data))
-            .catch( apiError => {
-                console.log(apiError)
+        const checkUserToken = token => {
+            return new Promise( ( resolve, reject ) => {
+                fetch( `${apiUrl}/api/me/${token}` )
+                .then( apiResponse => {
+                    return apiResponse.json()
+                })
+                .then(data => resolve(data))
+                .catch( apiError => reject(apiError))
             })
         }
+
         const getFormSumbit = () => {
             // Get registerForm submit
             registerForm.addEventListener('submit', event => {
@@ -84,9 +91,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 event.preventDefault();
 
                 // Check form data
-                searchData.value.length > 0 
-                ? fetchFunction(searchData.value) 
-                : displayError(searchData, 'Minimum 1 caractère');
+                if(searchData.value.length > 0){
+                    loading.classList.add('open'); 
+                    setTimeout(() => { fetchFunction(searchData.value) }, 600);
+                }
+                else{
+                    displayError(searchData, 'Minimum 1 caractère');
+                }
             });
         };
 
@@ -110,11 +121,48 @@ document.addEventListener('DOMContentLoaded', () => {
                 };
             })
             .then( jsonData => {
-                console.log(jsonData);
+                if( endpoint === 'login' ){
+                    // Add user ID in LocalStorage
+                    localStorage.setItem(localSt, jsonData.data.identity._id);
+
+                    checkUserToken(jsonData.data.identity._id)
+                    .then( apiResponse => {
+                        registerForm.classList.add('hidden');
+                        loginForm.classList.add('hidden');
+                        displayNav(apiResponse.data.user.pseudo)
+                        searchForm.classList.add('open');
+                        if( apiResponse.data.favorite.length > 0 ){
+                            displayFavorite(apiResponse.data.favorite)
+                        }
+                    })
+                    .catch( err => {
+                        console.log(err)
+                    } )
+                }
+                else if( endpoint === 'favorite' ){
+                    console.log(jsonData)
+                    checkUserToken(localStorage.getItem(localSt))
+                    .then( apiResponse => {
+                        displayFavorite(apiResponse.data.favorite)
+                    } )
+                }
             })
             .catch( apiError => {
                 console.error(apiError);
             });
+        }
+
+        const deleteFetch = id => {
+            console.log(id)
+            fetch( `${apiUrl}/api/favorite/${id}`, {
+                method: 'DELETE'
+            })
+            .then( apiResponse => {
+                console.log(apiResponse)
+            })
+            .catch( apiError => {
+                console.log(apiError)
+            })
         }
 
         const displayError = (tag, msg) => {
@@ -124,6 +172,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const fetchFunction = (keywords, index = 1) => {
             
+
             let fetchUrl = null;
 
             typeof keywords === 'number' 
@@ -146,25 +195,20 @@ document.addEventListener('DOMContentLoaded', () => {
             movieList.innerHTML = '';
 
             for( let i = 0; i < collection.length; i++ ){
+                let cover = collection[i].poster_path !== null ? 'https://image.tmdb.org/t/p/w500/' + collection[i].poster_path : './img/blankCover.jpg'
                 movieList.innerHTML += `
                     <article>
                         <figure>
-                            <img src="https://image.tmdb.org/t/p/w500/${collection[i].poster_path}" alt="${collection[i].original_title}">
-                            <figcaption movie-id="${collection[i].id}">
-                                ${collection[i].original_title} (voir plus...)
-                            </figcaption>
+                            <img src="${cover}" alt="${collection[i].original_title}">
+                            <figcaption movie-id="${collection[i].id}">${collection[i].original_title}</figcaption>
                         </figure>
-                        <div class="overview">
-                            <div>
-                                <p>${collection[i].overview}</p>
-                                <button>Voir le film</button>
-                            </div>
-                        </div>
                     </article>
                 `;
             };
 
             getPopinLink( document.querySelectorAll('figcaption') );
+            closeLoading();
+            
         };
 
         const getPopinLink = linkCollection => {
@@ -178,37 +222,151 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const displayPopin = data => {
             console.log(data);
-            moviePopin.innerHTML = `
-                <div>
-                    <img src="https://image.tmdb.org/t/p/w500/${data.poster_path}" alt="${data.original_title}">
-                </div>
+            let productions = '';
+            for( let item of data.production_companies ){ productions += `<span>${item.name}</span>` }
 
-                <div>
-                    <h2>${data.original_title}</h2>
-                    <p>${data.overview}</p>
-                    <button>Voir en streaming</button>
-                    <button id="closeButton">Close</button>
-                </div>
-            `;
+            if( localStorage.getItem(localSt) !== null ){
+                let cover = data.poster_path !== null ? 'https://image.tmdb.org/t/p/w500/' + data.poster_path : './img/blankCover.jpg'
+                moviePopin.innerHTML = `
+                    <div>
+                        <img src="${cover }" alt="${data.original_title}">
+                    </div>
+
+                    <div>
+                        <h2>${data.original_title} <b>${productions}</b></h2>
+                        <p id="movieTagline">${data.overview}</p>
+                        <p id="movieVote"><b>${data.vote_average}/10</b> <span>for ${data.vote_count} votes</span></p>
+                        <button><i class="fas fa-ticket-alt"></i> <span>Get the stream</span></button>
+                        <button id="favoriteButton"><i class="fas fa-bookmark"></i></button>
+                        <button id="closeButton"><i class="fas fa-times"></i></button>
+                    </div>
+                `;
+                addFavorite(document.querySelector('#favoriteButton'), data)
+            }
+            else{
+                moviePopin.innerHTML = `
+                    <div>
+                        <img src="https://image.tmdb.org/t/p/w500/${data.poster_path}" alt="${data.original_title}">
+                    </div>
+
+                    <div>
+                        <h2>${data.original_title}</h2>
+                        <p>${data.overview}</p>
+                        <button><i class="fas fa-ticket-alt"></i> <span>Voir le film</span></button>
+                        <button id="closeButton"><i class="fas fa-times"></i></button>
+                    </div>
+                `;
+            }
 
             moviePopin.parentElement.classList.add('open');
             closePopin( document.querySelector('#closeButton') )
+            
         };
 
         const closePopin = button => {
             button.addEventListener('click', () => {
-                button.parentElement.parentElement.parentElement.classList.add('close');
-                setTimeout( () => {
-                    button.parentElement.parentElement.parentElement.classList.remove('open');
-                    button.parentElement.parentElement.parentElement.classList.remove('close');
-                }, 300 )
+                button.parentElement.parentElement.parentElement.classList.remove('open');
             })
+        }
+
+        const displayNav = pseudo => {
+            mainNav.innerHTML = `
+                <p>Hello ${pseudo}</p>
+                <button id="logoutBtn"><i class="fas fa-sign-out-alt"></i></button>
+            `;
+
+            mainNav.classList.remove('hidden')
+
+            document.querySelector('#logoutBtn').addEventListener('click', () => {
+                // Delete LocalStorage
+                localStorage.removeItem(localSt);
+                mainNav.innerHTML= '';
+                favoriteList.innerHTML= '';
+                registerForm.classList.remove('hidden');
+                loginForm.classList.remove('hidden');
+                favoriteList.classList.remove('open');
+                favorite.classList.remove('open');
+                searchForm.classList.remove('open');
+            })
+        }
+
+        const addFavorite = (tag, data) => {
+            tag.addEventListener('click', () => {
+                postFetch('favorite', { 
+                    author: localStorage.getItem(localSt),
+                    id: data.id,
+                    title: data.original_title
+                })
+            })
+        }
+
+        const displayFavorite = data => {
+            favoriteList.innerHTML = '';
+            for(let item of data){
+                favoriteList.innerHTML += `
+                    <li>
+                        <button class="eraseFavorite" movie-id="${item.id}"><i class="fas fa-eraser"></i></button>
+                        <span  movie-id="${item.id}">${item.title}</span>
+                    </li>
+                `;
+            };
+            document.querySelector('#favorite').classList.add('open');
+            getPopinLink( document.querySelectorAll('#favorite li span') );
+            deleteFavorite(document.querySelectorAll('.eraseFavorite'))
+        }
+
+        const deleteFavorite = favorites => {
+            for( let item of favorites ){
+                item.addEventListener('click', () => {
+                    deleteFetch(item.getAttribute('movie-id'))
+                })
+            }
+        }
+
+        const closeLoading = () => {
+            loading.classList.add('close');
+            setTimeout(() => { 
+                loading.classList.remove('open');
+                loading.classList.remove('close');
+            }, 600);
         }
     //
 
     /* 
     Lancer IHM
     */
-        getFormSumbit();
+        if( localStorage.getItem(localSt) !== null ){
+            
+            checkUserToken(localStorage.getItem(localSt))
+            .then( apiResponse => {
+                if( apiResponse.data.favorite.length > 0 ){
+                    displayFavorite(apiResponse.data.favorite)
+                }
+
+                registerForm.classList.add('hidden');
+                loginForm.classList.add('hidden');
+                searchForm.classList.add('open');
+                displayNav(apiResponse.data.user.pseudo)
+                getFormSumbit();
+            })
+            .catch( err => {
+                console.log(err)
+                getFormSumbit();
+            } )
+        }
+        else{
+            getFormSumbit();
+        }
+
+        // https://vsrequest.video/request.php?key=oh61giHx16GXKSIz&secret_key=h9oe5ezesk0c0f9zk143zpkd7tba63&video_id=*VIDEO_ID*&tmdb=*TMDB*&tv=*TV*&s=*SEASON_NUMBER*&ip=*VISITOR_IP* http://127.0.0.1:8080
+
+        fetch('https://vsrequest.video/request.php?key=oh61giHx16GXKSIz&secret_key=h9oe5ezesk0c0f9zk143zpkd7tba63&video_id=348&tmdb=1&ip=77.204.106.82')
+        .then( spiderVideoResponse => {
+            console.log(spiderVideoResponse)
+        })
+        .catch( spiderVideoError => {
+            console.log(spiderVideoError)
+        })
+        
     //
 });
